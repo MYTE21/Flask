dc5d3c00812e0edb9b531e1ec5dd93a761c24290
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2022-08-22 01:41:30 +0600

    delete task

--- app.py
+++ app.py
@@ -35,6 +35,18 @@ def index():
         return render_template('index.html', tasks=tasks)
 
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except Exception:
+        return 'There was a problem deleting the task...!'
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)

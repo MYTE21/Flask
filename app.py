f5a8045625a2e9c6d5d54806576e7dc51cb53a4d
MYTE <yasmi.tohabar@gmail.com>
2023-05-03 17:57:13 +0600

    Update messages of the application

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
+"""
+Flask: An instance of this class will be our WSGI application.
+render_template: render_template is used to generate output from a template file.
+request: The requests library is for your app to make HTTP request.
+redirect: Returns a response object that, if called, redirects the client to the target location.
+"""
 from flask import Flask, render_template, request, redirect
+# SQLAlchemy: An SQL toolkit that provides efficient and high-performing database access for relational databases.
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime   # datetime: Work with dates and times.
 
-app = Flask(__name__)
+
+app = Flask(__name__)   # creates a Flask application object 'app' in the current Python module.
+# SQLALCHEMY_DATABASE_URI: The database URI that should be used for the connection.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
+# Create a model class
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
-    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -27,8 +36,9 @@ def index():
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-        except Exception:
-            return 'There was an issue adding your task...!'
+        except Exception as ex:
+            ex_message = ex.args[0]
+            return 'There was an issue adding your task...!', ex_message
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
@@ -43,8 +53,9 @@ def delete(id):
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
-    except Exception:
-        return 'There was a problem deleting the task...!'
+    except Exception as ex:
+        ex_message = ex.args[0]
+        return 'There was a problem deleting the task...!', ex_message
 
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -57,8 +68,9 @@ def update(id):
         try:
             db.session.commit()
             return redirect('/')
-        except Exception:
-            return 'There was a problem updating the task'
+        except Exception as ex:
+            ex_message = ex.args[0]
+            return 'There was a problem updating the task', ex_message
     else:
         return render_template('update.html', task=task)
 
